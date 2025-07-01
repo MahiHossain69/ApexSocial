82c2faf78d59cf84e3f1418157a61009b801d13e
MahiHossain69 <mahihossain8th@gmail.com>
2025-07-01 09:28:31 -1200

    unlink profile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -55,13 +55,13 @@ const Navbar = () => {
               <div className="absolute top-12 right-0 bg-white border border-gray-200 rounded-lg shadow-lg w-40 z-50">
                 <ul className="py-2 text-sm text-gray-700">
                   <li className='font-inter font-semibold'>
-                    <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">Profile</Link>
+                    <Link to="#" className="block px-4 py-2 hover:bg-gray-100">Profile</Link>
                   </li>
                   <li className='font-inter font-semibold'>
-                    <Link to="/settings" className="block px-4 py-2 hover:bg-gray-100">Settings</Link>
+                    <Link to="#" className="block px-4 py-2 hover:bg-gray-100">Settings</Link>
                   </li>
                   <li className='font-inter font-semibold'>
-                    <Link to="/logout" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
+                    <Link to="#" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
                   </li>
                 </ul>
               </div>
