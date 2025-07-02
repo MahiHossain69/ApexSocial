c1507bc370925b09da9d61d69eb93c2ac988b1ac
MahiHossain69 <mahihossain8th@gmail.com>
2025-07-01 14:59:41 -1200

    link update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
               <div className="absolute top-12 right-0 bg-white border border-gray-200 rounded-lg shadow-lg w-40 z-50">
                 <ul className="py-2 text-sm text-gray-700">
                   <li className='font-inter font-semibold'>
-                    <Link to="#" className="block px-4 py-2 hover:bg-gray-100">Profile</Link>
+                    <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">Profile</Link>
                   </li>
                   <li className='font-inter font-semibold'>
                     <Link to="#" className="block px-4 py-2 hover:bg-gray-100">Settings</Link>
